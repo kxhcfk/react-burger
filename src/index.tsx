19679054a06d5a00a7dd368aa9e2819984a9828e
Maksim Sirotkin <reveersed@mail.ru>
2023-08-20 12:41:35 +0300

    router fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import App from "./components/App/App";
-import {Provider} from "react-redux";
-import {store} from "./store/store";
-import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement,
 );
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <HashRouter>
+            <BrowserRouter basename="/react-burger">
                 <App/>
-            </HashRouter>
+            </BrowserRouter>
         </Provider>
-    </React.StrictMode>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your App, pass a function
